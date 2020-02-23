e7b0dd4b0caeddf0f7b7cd52a59ee1b426fe265b
Billiam <billiamthesecond@gmail.com>
2020-02-23 00:12:13 -0600

    Add repository class, database initialization

--- lib/group_parser.rb
+++ lib/group_parser.rb
@@ -1,2 +1,3 @@
 require 'group_parser/group_parser'
 require 'group_parser/message'
+require 'group_parser/repository'
---
+++ lib/group_parser/repository.rb
@@ -0,0 +1,48 @@
+require 'sqlite3'
+
+module GroupParser
+  class Repository
+    def initialize(path)
+      @db = SQLite3::Database.new path
+    end
+
+    def setup
+      @db.execute_batch <<-SQL
+        PRAGMA journal_mode = MEMORY;
+
+        create TABLE IF NOT EXISTS meta (
+          version INTEGER PRIMARY KEY
+        );
+
+        CREATE TABLE IF NOT EXISTS messages (
+          id TEXT PRIMARY KEY,
+          date INTEGER,
+          from_display TEXT,
+          from_address TEXT,
+          subject TEXT,
+          body TEXT
+        );
+        CREATE INDEX IF NOT EXISTS from_index
+          ON messages (from_address);
+
+        CREATE INDEX IF NOT EXISTS date_index
+          ON messages (date ASC);
+      SQL
+    end
+
+    def with_transaction
+      @db.execute('begin')
+      yield
+      @db.execute('end')
+    end
+
+    def store(message)
+      query = <<-SQL
+        INSERT INTO messages (id, date, from_display, from_address, subject, body)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ON CONFLICT (id) DO NOTHING
+      SQL
+      @db.execute(query, [message.id, message.date.to_i, message.from, message.from_email, message.subject, message.body])
+    end
+  end
+end
